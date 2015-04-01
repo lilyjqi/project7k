bb943223e816e18d0c0cf8a9e53b0e2f8fd3d4a4
lilyjqi <lilyjqi@gmail.com>
2015-04-01 17:47:56 -0400

    Create ABDecorator.h

---
+++ ABDecorator.h
@@ -0,0 +1,17 @@
+#ifndef __ABDECORATOR_H__
+#define __ABDECORATOR_H__ 
+
+#include <iostream>
+#include <cstdlib>
+#include "acadBuilding.h"
+
+class ABDecorator: public AcadBuilding
+{
+protected:
+	AcadBuilding* ab;
+	ABDecorator(AcadBuilding *ab): ab(ab) {}
+	~ABDecorator() {delete ab};
+
+};
+
+#endif
