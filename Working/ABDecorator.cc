15da192f6bb7900ac0e00ecaf310d8e8e0158683
lilyjqi <lilyjqi@gmail.com>
2015-04-04 17:33:50 -0400

    update

--- Working/ABDecorator.cc
+++ Working/ABDecorator.cc
@@ -4,4 +4,4 @@ using namespace std;
 
 ABDecorator::ABDecorator(AcadBuilding *ab): AcadBuilding(ab->getName(), ab->getIndex(), ab->getRow(), ab->getCol(), ab->getmonoBlock()), ab(ab) {}
 
-~ABDecorator() {}
+ABDecorator::~ABDecorator() {}
