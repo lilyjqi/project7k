55a10048fc5012bdcea22db3f81e153f73cc79d8
normanwang92 <whz-man@hotmail.com>
2015-04-03 20:53:25 -0400

    Update building.cc
    default owner to school

--- Working/building.cc
+++ Working/building.cc
@@ -1,8 +1,9 @@
 #include "building.h"
+#include "school.h"
 
 Building::Building(string name, int position, int rindex, int cindex, int numNeighbours):
     Tile(name, position, rindex, cindex),
-    numNeighbours(numNeighbours),neighbours(NULL),cost(0),mort(false) {}
+    numNeighbours(numNeighbours),neighbours(NULL),cost(0),mort(false),o(School::getInstance()) {}
 
 void Building::setOwner(Owner * o) {
     owner = o;
