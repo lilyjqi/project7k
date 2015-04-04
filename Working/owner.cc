5869002af705a450d76607d3b754b379c04f3a04
normanwang92 <whz-man@hotmail.com>
2015-04-04 11:44:28 -0400

    Update owner.cc
    changes in ctor initializing vector ownbuilding

--- Working/owner.cc
+++ Working/owner.cc
@@ -4,9 +4,9 @@
 using namespace std;
 
 Owner::Owner(string name): 
-    name(name), balance(1500), numBuilding(0) {
-        ownBuilding = new Building *[28];
-    };
+    name(name), balance(1500), numBuilding(0), ownBuilding(vector <Buidling* >) {
+//        ownBuilding = new Building *[28];
+}
 
 string Owner::getName() {
     return name;
